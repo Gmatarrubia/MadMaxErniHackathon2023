e84e82273bd6b995ccaadb70c6f96f3172c70355
EU\anegon-1 <fernando.anegon@mt.com>
2023-09-22 11:58:07 +0200

    First component layer: status, control & errors

--- MadMaxWeb/src/app/app.module.ts
+++ MadMaxWeb/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StatusComponent } from './status/status.component';
+import { ControlComponent } from './control/control.component';
+import { ErrorsComponent } from './errors/errors.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StatusComponent,
+    ControlComponent,
+    ErrorsComponent
   ],
   imports: [
     BrowserModule
