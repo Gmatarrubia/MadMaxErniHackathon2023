3a2a73bc31e59b8ee4d93453dcc160668d252d74
EU\anegon-1 <fernando.anegon@mt.com>
2023-09-23 15:31:52 +0200

    Timer activated with Start button

--- MadMaxWeb/src/app/status/status.component.ts
+++ MadMaxWeb/src/app/status/status.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { CommunicationService } from '../shared/communication.service';
+import { Automatic } from '../control/automatic/automatic.model';
 
 @Component({
   selector: 'app-status',
@@ -10,6 +11,9 @@ import { CommunicationService } from '../shared/communication.service';
 export class StatusComponent {
 
   statusFirebaseData: any;
+  automaticFirebaseData: any;
+  timer: number = 0;
+  timerRef: any;
 
   constructor(private communicationService: CommunicationService) { }
 
@@ -17,6 +21,20 @@ export class StatusComponent {
     const firebasePath = 'status';
     this.communicationService.getData(firebasePath).subscribe((data) => {
       this.statusFirebaseData = data;
+    })
+
+    this.communicationService.getData('automatic').subscribe((data) => {
+      this.automaticFirebaseData = data;
+      if (data.move) {
+        const startTime = Date.now() - (this.timer || 0);
+        this.timerRef = setInterval(() => {
+          this.timer = Date.now() - startTime;
+        })
+      } 
+      else {
+        this.timer = 0;
+        clearInterval(this.timerRef);
+      }
     })
   }
 }
