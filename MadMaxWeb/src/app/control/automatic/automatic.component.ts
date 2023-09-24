5063e4dd7953a8f5c84e37deabc69b6a231d3ae5
EU\anegon-1 <fernando.anegon@mt.com>
2023-09-24 13:28:56 +0200

    Comment unnecessary code

--- MadMaxWeb/src/app/control/automatic/automatic.component.ts
+++ MadMaxWeb/src/app/control/automatic/automatic.component.ts
@@ -24,19 +24,19 @@ export class AutomaticComponent {
 
   onStart() {
     this.communicationService.setAutomatic(new Automatic(1));
-    this.communicationService.setStatus(new Status("Started", 15, 5)); // Comment out!
+    // this.communicationService.setStatus(new Status("Started", 15, 5)); // Comment out!
     this.isStarted = true;
   }
 
   onPause() {
     this.communicationService.setAutomatic(new Automatic(2));
-    this.communicationService.setStatus(new Status("Paused", 0, 5)); // Comment out!
+    // this.communicationService.setStatus(new Status("Paused", 0, 5)); // Comment out!
     this.isStarted = false;
   }
 
   onStop() {
     this.communicationService.setAutomatic(new Automatic(0));
-    this.communicationService.setStatus(new Status("Stopped", 0, 1)); // Comment out!
+    // this.communicationService.setStatus(new Status("Stopped", 0, 1)); // Comment out!
     this.isStarted = false;
   }
 }
