650938307897c72e6467066b8def5c60dd98d09d
gmatarrubia <gonzalomatarrubia@hotmail.com>
2023-09-25 11:22:07 +0200

    communication module first

--- CommunicationModule/CommunicationModule.py
+++ CommunicationModule/CommunicationModule.py
@@ -8,6 +8,8 @@ import os
 import zmq
 
 CONTROL_MODE = 0  # Initialize with 0 (automatic)
+AUTOMATIC_MOVE = 0
+MANUAL_MOVE = 0
 PROGRESS = 0
 SPEED = 0
 STATUS = "paused"
@@ -41,12 +43,12 @@ def update_status(status_data):
     ref.update(status_data)
 
 # Function to start a listener thread
-def start_listener(collection_name, callback):
+def start_listener(collection_name, pubsock, callback):
     ref = db.reference(collection_name)
 
     def listener_callback(event):
         print(f"{collection_name} changed: {event.event_type}, New value: {event.data}")
-        callback(event)
+        callback(event, pubsock)
 
     listener_thread = threading.Thread(target=lambda: ref.listen(listener_callback))
     listener_thread.start()
@@ -77,20 +79,36 @@ def start_socket_read(socket):
     subscriber_thread.start()
 
 # ControlType listener callback
-def control_listener_callback(event):
+def control_listener_callback(event, pubSock):
     global CONTROL_MODE
-
     control_data = event.data
     CONTROL_MODE = control_data.get('type', 0) # Update CONTROL_MODE with the control_type
-    pass
+    pubSock.send_string(f"control_mode {CONTROL_MODE}")
 
 # Automatic listener callback
-def automatic_listener_callback(event):
-    pass
+def automatic_listener_callback(event, pubSock):
+    global AUTOMATIC_MOVE
+    automatic_data = event.data
+    AUTOMATIC_MOVE = automatic_data.get('move', 0) # Update CONTROL_MODE with the control_type
+    if CONTROL_MODE == 0:
+        pubSock.send_string(f"automatic_move {AUTOMATIC_MOVE}")
 
 # Manual listener callback
-def manual_listener_callback(event):
-    pass
+def manual_listener_callback(event, pubSock):
+    global MANUAL_MOVE
+    manual_data = event.data
+    if manual_data.get('down', 0) == True:
+        MANUAL_MOVE = "back"
+    elif manual_data.get('up', 0) == True:
+        MANUAL_MOVE = "fordward"
+    elif manual_data.get('left', 0) == True:
+        MANUAL_MOVE = "left"
+    elif manual_data.get("right", 0) == True:
+        MANUAL_MOVE = "right"
+    else:
+        MANUAL_MOVE = "stop"
+    if CONTROL_MODE == 1:
+        pubSock.send_string(f"manual_move {MANUAL_MOVE}")
 
 def stop_listeners():
     for listener_thread in active_listener_threads:
@@ -112,14 +130,12 @@ def main():
     set_initial_values()
 
     # Start all listeners
-    start_listener('controlType', control_listener_callback)
-    start_listener('automatic', automatic_listener_callback)
-    start_listener('manual', manual_listener_callback)
+    start_listener('controlType', fromWebSock, control_listener_callback)
+    start_listener('automatic', fromWebSock, automatic_listener_callback)
+    start_listener('manual', fromWebSock, manual_listener_callback)
 
     try:
         while True:
-            
-
             time.sleep(10)
 
     except KeyboardInterrupt:
