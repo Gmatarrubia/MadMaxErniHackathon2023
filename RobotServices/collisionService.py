4787d4f6e782fb692fa73c12fc6fc6dcb389dc8b
gmatarrubia <gonzalomatarrubia@hotmail.com>
2023-09-25 12:58:00 +0200

    minor fix

--- RobotServices/collisionService.py
+++ RobotServices/collisionService.py
@@ -20,13 +20,13 @@ def main():
     global PROGRESS
     try:
         while True:
-                distance = px.ultrasonic.read()
-                if distance < 300:
-                    OBJECT_DETECTED = 1
-                else:
-                    OBJECT_DETECTED = 0
-                pubSocket.send_string(f"object_detected {OBJECT_DETECTED}")
-                time.sleep(0.1)
+            distance = px.ultrasonic.read()
+            if distance < 300:
+                OBJECT_DETECTED = 1
+            else:
+                OBJECT_DETECTED = 0
+            pubSocket.send_string(f"object_detected {OBJECT_DETECTED}")
+            time.sleep(0.1)
 
     except KeyboardInterrupt:
         # Manually stop all active listener threads if you press Ctrl+C
